7974e7aa940c795b528904aae814a050ce1ca6a3
Esmeralda Iyescas <Esmeralda.Iyescas001@mymdc.net>
2020-04-08 18:01:00 -0400

    Help page now has a button to take the user back to home

--- Prototype Design/Form3.Designer.vb
+++ Prototype Design/Form3.Designer.vb
@@ -22,16 +22,29 @@ Partial Class Form3
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(627, 381)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(130, 28)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Return to Log in"
+        Me.Button1.UseVisualStyleBackColor = True
         '
         'Form3
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Button1)
         Me.Name = "Form3"
         Me.Text = "Help Page"
         Me.ResumeLayout(False)
 
     End Sub
+
+    Friend WithEvents Button1 As Button
 End Class
--- Prototype Design/Form3.vb
+++ Prototype Design/Form3.vb
@@ -1,3 +1,7 @@
 ﻿Public Class Form3
-
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim SecondForm As New Form1
+        SecondForm.Show()
+        Me.Hide()
+    End Sub
 End Class
