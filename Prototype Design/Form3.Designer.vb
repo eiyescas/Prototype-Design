edb2bb2000604b5eade6b6876b55549d6fcf8a48
Esmeralda Iyescas <Esmeralda.Iyescas001@mymdc.net>
2020-04-08 17:57:17 -0400

    Added a help form and renamed Form2

--- Prototype Design/Form3.Designer.vb
+++ Prototype Design/Form3.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class Form2
+Partial Class Form3
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
@@ -22,29 +22,16 @@ Partial Class Form2
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.btnLogOff = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
-        'btnLogOff
-        '
-        Me.btnLogOff.Location = New System.Drawing.Point(305, 448)
-        Me.btnLogOff.Name = "btnLogOff"
-        Me.btnLogOff.Size = New System.Drawing.Size(75, 23)
-        Me.btnLogOff.TabIndex = 0
-        Me.btnLogOff.Text = "Log Off"
-        Me.btnLogOff.UseVisualStyleBackColor = True
-        '
-        'Form2
+        'Form3
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(388, 490)
-        Me.Controls.Add(Me.btnLogOff)
-        Me.Name = "Form2"
-        Me.Text = "Citi Statements"
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Name = "Form3"
+        Me.Text = "Help Page"
         Me.ResumeLayout(False)
 
     End Sub
-
-    Friend WithEvents btnLogOff As Button
 End Class
